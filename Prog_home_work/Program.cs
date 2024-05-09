1535c7e44c0894568e4af0e741f4720ad0a81783
Denis B. <Deniscapital@mail.ru>
2024-05-09 16:57:50 +0300

    Написали программу, согласно ТЗ , с возможностью выбора количества строк и самостоятельно прописывать в строке длинну массива

--- Prog_home_work/Program.cs
+++ Prog_home_work/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    // Запрашиваем у пользователя количество строк
+    Console.Write("Введите количество строк: ");
+    int n = int.Parse(Console.ReadLine());
+
+    // Исходный массив строк
+    string[] strings = new string[n];
+
+    // Пользователь вводит строки
+    for (int i = 0; i < n; i++)
+    {
+      Console.Write($"Введите строку {i + 1}: ");
+      strings[i] = Console.ReadLine();
+    }
+
+    // Создаем новый массив для строк, длина которых <= 3
+    string[] newStrings = new string[strings.Length];
+
+    // Обходим все строки в исходном массиве
+    int j = 0;
+    for (int i = 0; i < strings.Length; i++)
+    {
+      // Если длина строки <= 3, добавляем ее в новый массив
+      if (strings[i].Length <= 3)
+      {
+        newStrings[j] = strings[i];
+        j++;
+      }
+    }
+
+    // Уменьшаем размер нового массива до фактического количества добавленных строк
+    Array.Resize(ref newStrings, j);
+
+    // Выводим новый массив
+    Console.WriteLine("Новый массив строк:");
+    foreach (string s in newStrings)
+    {
+      Console.WriteLine(s);
+    }
+  }
+}
